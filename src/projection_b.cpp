7b175e70b34adeac6ebf9a96062b4cdc715f0078
MickeyKen <mickey333ff@gmail.com>
2019-10-17 05:16:28 +0900

    B and C point

--- src/projection_b.cpp
+++ src/projection_b.cpp
@@ -24,7 +24,7 @@ void Callback(const std_msgs::Int16& msg)
   ros::Rate rate(20);
 
 
-  if (exp_num >= 1 && exp_num <= 4) {
+  if (exp_num >= 5 && exp_num <= 8) {
     ///// decide image size in real world
     float size = 800 / 2;
     ///// get image and resize projectr size
@@ -43,16 +43,16 @@ void Callback(const std_msgs::Int16& msg)
       }
       cv::Mat warp_img(cv::Size(1024, 768), CV_8U, cv::Scalar::all(0));
       resize(source_img, source_img, cv::Size(ColumnOfNewImage,RowsOfNewImage));
-      cv::Mat M = (cv::Mat_<double>(3,3) << 0.1328612051691586, 0.8424816092172287, 186.7617340087891, -0.1048375316667271, 0.001463219311317654, 486.4222412109378, 0.0002541468389892483, -3.799551891974472e-06, 1);
+      cv::Mat M = (cv::Mat_<double>(3,3) << -0.3534832982070039, 0.7253135815968155, 510.0434265136709, -0.06815942484068727, -0.08903308677962241, 524.5535888671869, 0.00018416658063777, 0.0002401983129224792, 1);
       cv::warpPerspective( source_img, warp_img, M, source_img.size());
-      cv::namedWindow( "screen_24" );
-      cv::setWindowProperty("screen_24",CV_WND_PROP_FULLSCREEN,CV_WINDOW_FULLSCREEN);
-      cv::imshow("screen_24", warp_img);
+      cv::namedWindow( "screen_b" );
+      cv::setWindowProperty("screen_b",CV_WND_PROP_FULLSCREEN,CV_WINDOW_FULLSCREEN);
+      cv::imshow("screen_b", warp_img);
       cv::waitKey(1);
 
       rate.sleep();
       }
-      cv::destroyWindow("screen_24");
+      cv::destroyWindow("screen_b");
 
 
     }
@@ -62,7 +62,7 @@ void Callback(const std_msgs::Int16& msg)
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "exp_1234");
+  ros::init(argc, argv, "exp_5678");
 
   ros::NodeHandle n;
 
--- src/projection_c.cpp
+++ src/projection_c.cpp
@@ -24,7 +24,7 @@ void Callback(const std_msgs::Int16& msg)
   ros::Rate rate(20);
 
 
-  if (exp_num >= 1 && exp_num <= 4) {
+  if (exp_num >= 9 && exp_num <= 12) {
     ///// decide image size in real world
     float size = 800 / 2;
     ///// get image and resize projectr size
@@ -43,16 +43,16 @@ void Callback(const std_msgs::Int16& msg)
       }
       cv::Mat warp_img(cv::Size(1024, 768), CV_8U, cv::Scalar::all(0));
       resize(source_img, source_img, cv::Size(ColumnOfNewImage,RowsOfNewImage));
-      cv::Mat M = (cv::Mat_<double>(3,3) << 0.1328612051691586, 0.8424816092172287, 186.7617340087891, -0.1048375316667271, 0.001463219311317654, 486.4222412109378, 0.0002541468389892483, -3.799551891974472e-06, 1);
+      cv::Mat M = (cv::Mat_<double>(3,3) << -0.46373082766843, -0.391328023159111, 901.1663818359388, 0.05576542651957173, -0.07783323985820123, 386.7452392578139, -0.0001504435708675367, 0.0002103010190235412, 1);
       cv::warpPerspective( source_img, warp_img, M, source_img.size());
-      cv::namedWindow( "screen_24" );
-      cv::setWindowProperty("screen_24",CV_WND_PROP_FULLSCREEN,CV_WINDOW_FULLSCREEN);
-      cv::imshow("screen_24", warp_img);
+      cv::namedWindow( "screen_c" );
+      cv::setWindowProperty("screen_c",CV_WND_PROP_FULLSCREEN,CV_WINDOW_FULLSCREEN);
+      cv::imshow("screen_c", warp_img);
       cv::waitKey(1);
 
       rate.sleep();
       }
-      cv::destroyWindow("screen_24");
+      cv::destroyWindow("screen_c");
 
 
     }
@@ -62,7 +62,7 @@ void Callback(const std_msgs::Int16& msg)
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "exp_1234");
+  ros::init(argc, argv, "exp_9101112");
 
   ros::NodeHandle n;
 
