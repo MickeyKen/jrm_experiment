16dc3f2a459eabf3d86535387d82e8786ea23a2b
MickeyKen <mickey333ff@gmail.com>
2019-10-21 17:56:51 +0900

    [Fix] make

--- src/projector_for_video.cpp
+++ src/projector_for_video.cpp
@@ -25,7 +25,7 @@ void Callback(const std_msgs::Int16& msg)
   n.setParam("exp_miki_img/switch", 1);
 
 
-  if (exp_num == 1 || exp_num == 2) {
+  if (exp_num == 91 || exp_num == 92 || exp_num == 93) {
     ///// decide image size in real world
     float size = 800 / 2;
     int ran = rand() % 10;
@@ -125,73 +125,72 @@ void Callback(const std_msgs::Int16& msg)
       m.getRPY(roll, pitch, yaw);
       // std::cout << "Roll: " << roll << ", Pitch: " << pitch << ", Yaw: " << yaw << std::endl;
 
-      if (roll > -2.5000 && roll < -2.25000) {
 
-        ///// calcurate Rotation Matrix
-        // insert Rotation matrix for X
-        rot_x.at<float>(1, 1) = cos(roll);
-        rot_x.at<float>(1, 2) = -sin(roll);
-        rot_x.at<float>(2, 1) = sin(roll);
-        rot_x.at<float>(2, 2) = cos(roll);
+      ///// calcurate Rotation Matrix
+      // insert Rotation matrix for X
+      rot_x.at<float>(1, 1) = cos(roll);
+      rot_x.at<float>(1, 2) = -sin(roll);
+      rot_x.at<float>(2, 1) = sin(roll);
+      rot_x.at<float>(2, 2) = cos(roll);
 
-        //insert Ritation matrix for y
-        rot_y.at<float>(0, 0) = cos(pitch);
-        rot_y.at<float>(0, 2) = sin(pitch);
-        rot_y.at<float>(2, 0) = -sin(pitch);
-        rot_y.at<float>(2, 2) = cos(pitch);
+      //insert Ritation matrix for y
+      rot_y.at<float>(0, 0) = cos(pitch);
+      rot_y.at<float>(0, 2) = sin(pitch);
+      rot_y.at<float>(2, 0) = -sin(pitch);
+      rot_y.at<float>(2, 2) = cos(pitch);
 
-        //insert Rotation matrix for z
-        rot_z.at<float>(0, 0) = cos(yaw);
-        rot_z.at<float>(0, 1) = -sin(yaw);
-        rot_z.at<float>(1, 0) = sin(yaw);
-        rot_z.at<float>(1, 1) = cos(yaw);
+      //insert Rotation matrix for z
+      rot_z.at<float>(0, 0) = cos(yaw);
+      rot_z.at<float>(0, 1) = -sin(yaw);
+      rot_z.at<float>(1, 0) = sin(yaw);
+      rot_z.at<float>(1, 1) = cos(yaw);
 
-        Rotation = rot_z * rot_y * rot_x;
+      Rotation = rot_z * rot_y * rot_x;
 
-        Rotation.at<float>(0, 2) = transform.getOrigin().x()*1000;
-        Rotation.at<float>(1, 2) = transform.getOrigin().y()*1000;
-        Rotation.at<float>(2, 2) = transform.getOrigin().z()*1000;
-        // std::cout << "cmoplete:" << Rotation << std::endl;
+      Rotation.at<float>(0, 2) = transform.getOrigin().x()*1000;
+      Rotation.at<float>(1, 2) = transform.getOrigin().y()*1000;
+      Rotation.at<float>(2, 2) = transform.getOrigin().z()*1000;
+      // std::cout << "cmoplete:" << Rotation << std::endl;
 
-        ///// calcurate center x-y-z point in real world
-        calc = (Ap * Rotation).inv() * uv_center;
+      ///// calcurate center x-y-z point in real world
+      calc = (Ap * Rotation).inv() * uv_center;
 
-        calc = calc / calc.at<float>(2,0);
-        // std::cout << "cmoplete:" << calc << std::endl;
+      calc = calc / calc.at<float>(2,0);
+      // std::cout << "cmoplete:" << calc << std::endl;
 
-        target.at<float>(0,0) = calc.at<float>(0,0) - size;
-        target.at<float>(0,1) = calc.at<float>(1,0) + size;
-        target.at<float>(0,2) = 1.>0;
+      target.at<float>(0,0) = calc.at<float>(0,0) - size;
+      target.at<float>(0,1) = calc.at<float>(1,0) + size;
+      target.at<float>(0,2) = 1.>0;
 
-        target.at<float>(1,0) = calc.at<float>(0,0) + size;
-        target.at<float>(1,1) = calc.at<float>(1,0) + size;
-        target.at<float>(1,2) = 1.0;
+      target.at<float>(1,0) = calc.at<float>(0,0) + size;
+      target.at<float>(1,1) = calc.at<float>(1,0) + size;
+      target.at<float>(1,2) = 1.0;
 
-        target.at<float>(2,0) = calc.at<float>(0,0) + size;
-        target.at<float>(2,1) = calc.at<float>(1,0) - size;
-        target.at<float>(2,2) = 1.0;
+      target.at<float>(2,0) = calc.at<float>(0,0) + size;
+      target.at<float>(2,1) = calc.at<float>(1,0) - size;
+      target.at<float>(2,2) = 1.0;
 
-        target.at<float>(3,0) = calc.at<float>(0,0) - size;
-        target.at<float>(3,1) = calc.at<float>(1,0) - size;
-        target.at<float>(3,2) = 1.0;
+      target.at<float>(3,0) = calc.at<float>(0,0) - size;
+      target.at<float>(3,1) = calc.at<float>(1,0) - size;
+      target.at<float>(3,2) = 1.0;
 
-        for (int i = 0; i < 4; i++) {
-          calc =  Ap * Rotation * target.row(i).t();
-          dst_pt[i].x = calc.at<float>(0,0) / calc.at<float>(2,0);
-          dst_pt[i].y = calc.at<float>(1,0) / calc.at<float>(2,0);
-          // printf("x: %f , y: %f", dst_pt[i].x, dst_pt[i].y);
-          // std::cout << "g = "<< std::endl << " "  << Ap * Rotation * target.row(i).t() << std::endl << std::endl;
-        }
-        cv::Mat M = cv::getPerspectiveTransform(src_pt,dst_pt);
-        cv::warpPerspective( source_img, warp_img, M, source_img.size());
-        // std::cout << "g = "<< std::endl << " "  << M << std::endl << std::endl;
-        ///// set window fullscreen
-        cv::namedWindow( "screen_24", CV_WINDOW_NORMAL );
-        cv::setWindowProperty("screen_24",CV_WND_PROP_FULLSCREEN,CV_WINDOW_FULLSCREEN);
-        cv::imshow("screen_24", warp_img);
-        cv::waitKey(1);
-
+      for (int i = 0; i < 4; i++) {
+        calc =  Ap * Rotation * target.row(i).t();
+        dst_pt[i].x = calc.at<float>(0,0) / calc.at<float>(2,0);
+        dst_pt[i].y = calc.at<float>(1,0) / calc.at<float>(2,0);
+        // printf("x: %f , y: %f", dst_pt[i].x, dst_pt[i].y);
+        // std::cout << "g = "<< std::endl << " "  << Ap * Rotation * target.row(i).t() << std::endl << std::endl;
       }
+      cv::Mat M = cv::getPerspectiveTransform(src_pt,dst_pt);
+      cv::warpPerspective( source_img, warp_img, M, source_img.size());
+      // std::cout << "g = "<< std::endl << " "  << M << std::endl << std::endl;
+      ///// set window fullscreen
+      cv::namedWindow( "screen_24", CV_WINDOW_NORMAL );
+      cv::setWindowProperty("screen_24",CV_WND_PROP_FULLSCREEN,CV_WINDOW_FULLSCREEN);
+      cv::imshow("screen_24", warp_img);
+      cv::waitKey(1);
+
+
 
 
 
